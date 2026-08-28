a0e92aeadb370ccdec071c3fc0d53f383b15de97
Erick <erick2ribeirogg@gmail.com>
2026-08-28 19:13:06 -0300

    feat: initialize FastAPI application and register auth and activities routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
+import sys
+from pathlib import Path
+import uvicorn
 from fastapi import FastAPI
 
-from app.routes import activities, auth
+# Garante que o diretório raiz 'backend' esteja no sys.path
+backend_dir = Path(__file__).resolve().parent.parent
+if str(backend_dir) not in sys.path:
+    sys.path.insert(0, str(backend_dir))
 
+from app.routes import activities, auth
 
 app = FastAPI()
 
@@ -11,3 +18,8 @@ app.include_router(activities.router)
 @app.get("/")
 def home():
     return {"message": "Strava analyzer funcionando"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+
